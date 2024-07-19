5d28c822daf3fb4ab0b929092f05d2d3004f4eca
Jorge Lopez <lopez.cs.jorge@gmail.com>
2024-07-18 22:43:41 -0300

    I had problems with the markup language and I fixed it

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 function sumar() {
-  var num1 = parseInt(document.getElementById("num1").value);
-  var num2 = parseInt(document.getElementById("num2").value);
-  var resultado = num1 + num2;
+  let num1 = parseInt(document.getElementById("num1").value);
+  let num2 = parseInt(document.getElementById("num2").value);
+  let resultado = num1 + num2;
   document.getElementById("resultado").value = resultado;
 }
 
 function restar() {
-  var num1 = parseInt(document.getElementById("num1").value);
-  var num2 = parseInt(document.getElementById("num2").value);
-  var resultado = num1 - num2;
+  let num1 = parseInt(document.getElementById("num1").value);
+  let num2 = parseInt(document.getElementById("num2").value);
+  let resultado = num1 - num2;
   document.getElementById("resultado").value = resultado;
 }
 
 function multiplicar() {
-  var num1 = parseInt(document.getElementById("num1").value);
-  var num2 = parseInt(document.getElementById("num2").value);
-  var resultado = num1 * num2;
+  let num1 = parseInt(document.getElementById("num1").value);
+  let num2 = parseInt(document.getElementById("num2").value);
+  let resultado = num1 * num2;
   document.getElementById("resultado").value = resultado;
 }
 
 function dividir() {
-  var num1 = parseInt(document.getElementById("num1").value);
-  var num2 = parseInt(document.getElementById("num2").value);
-  var resultado = num1 / num2;
+  let num1 = parseInt(document.getElementById("num1").value);
+  let num2 = parseInt(document.getElementById("num2").value);
+  let resultado = num1 / num2;
   document.getElementById("resultado").value = resultado;
 }
 
